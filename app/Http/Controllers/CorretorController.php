382cfbc1607cbab25518cc6cecf97eaa97d9603a
Paulo <pauloloppescoelho@gmail.com>
2019-10-09 21:22:30 -0300

    Criou Model e Controller

---
+++ app/Http/Controllers/CorretorController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Corretor;
+use Illuminate\Http\Request;
+
+class CorretorController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Corretor  $corretor
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Corretor $corretor)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Corretor  $corretor
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Corretor $corretor)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Corretor  $corretor
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Corretor $corretor)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Corretor  $corretor
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Corretor $corretor)
+    {
+        //
+    }
+}
